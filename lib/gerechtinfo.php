6648a979d6c02b17d6d84fd92fffc68a53e92fc5
Raf <raferriu@hotmail.com>
2021-09-16 14:11:31 +0200

    16/09/2021

--- lib/gerechtinfo.php
+++ lib/gerechtinfo.php
@@ -32,8 +32,4 @@ class gerecht_info {
     }
 
 
-    public function toevoegenFavoriet($recept_id, $user_id) {
-         $sql = "INSERT INTO gerecht_info (recept_id, user_id, record_type) VALUES ($recept_id, $user_id, 'F')";
-         $result = mysqli_query($this->connectie, $sql);
-    }
 }
